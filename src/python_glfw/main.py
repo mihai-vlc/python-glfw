dde5cfd6bacd6530e1ca361218938a2bae8d3039
Mihai Ionut Vilcu <ionutvmi@gmail.com>
2023-04-17 16:35:49 +0300

    Moves the graph into a separate module

--- src/python_glfw/main.py
+++ src/python_glfw/main.py
@@ -1,75 +1,9 @@
 from typing import Any
 import glfw
 import moderngl
-import numpy as np
 import math
 from python_glfw.logger import logger
-
-def vertices():
-    x = np.linspace(-1.0, 1.0, 50)
-    y = np.random.rand(50) - 0.5
-    r = np.ones(50)
-    g = np.arange(0, 1, 0.02)
-    b = np.zeros(50)
-    a = np.zeros(50)
-    return np.dstack([x, y, r, g, b, a])
-
-class HelloWorld2D:
-    def __init__(self, ctx: moderngl.Context, reserve: str='4MB'):
-        self.ctx = ctx
-        self.prog = self.ctx.program(
-            vertex_shader='''
-                #version 330
-
-                uniform vec2 Pan;
-
-                in vec2 in_vert;
-                in vec4 in_color;
-
-                out vec4 v_color;
-
-                void main() {
-                    v_color = in_color;
-                    gl_Position = vec4(in_vert - Pan, 0.0, 1.0);
-                }
-            ''',
-            fragment_shader='''
-                #version 330
-
-                in vec4 v_color;
-
-                out vec4 f_color;
-
-                void main() {
-                    f_color = v_color;
-                }
-            ''',
-        )
-
-        self.vbo = ctx.buffer(reserve=reserve, dynamic=True)
-        self.vao = ctx.simple_vertex_array(self.prog, self.vbo, 'in_vert', 'in_color')
-
-    def pan(self, pos):
-        self.prog['Pan'].value = pos
-
-    def relative_pan(self, relative_pos):
-        pos = self.prog['Pan'].value
-        self.prog['Pan'].value = (pos[0] + relative_pos[0], pos[1] + relative_pos[1])
-
-    def clear(self, color=(0, 0, 0, 0)):
-        self.ctx.clear(*color)
-
-    def plot(self, points, type='line'):
-        data = points.astype('f4').tobytes()
-        self.vbo.orphan()
-        self.vbo.write(data)
-        if type == 'line':
-            self.ctx.line_width = 5.0
-            self.vao.render(moderngl.LINE_STRIP, vertices=len(data) // 24)
-        if type == 'points':
-            self.ctx.point_size = 3.0
-            self.vao.render(moderngl.POINTS, vertices=len(data) // 24)
-
+from python_glfw.screens import Graph2DScreen
 
 
 def run():
@@ -90,27 +24,22 @@ def run():
     glfw.swap_interval(1)
 
     ctx = moderngl.create_context()
-    canvas = HelloWorld2D(ctx)
-    verts = vertices()
-
+    canvas = Graph2DScreen(ctx)
 
     def key_callback(window: Any, key: int, scancode: int, action: int, mods: int):
-        nonlocal verts
         if action == glfw.RELEASE:
             return
 
         if key == glfw.KEY_LEFT:
             canvas.relative_pan((-0.1, 0))
-        
+
         if key == glfw.KEY_RIGHT:
             canvas.relative_pan((0.1, 0))
 
         if key == glfw.KEY_ENTER:
-            verts = vertices()
-        
+            canvas.regenerate()
 
     glfw.set_key_callback(window, key_callback)
-
 
     def window_size_callback(window: Any, w: int, h: int):
         ctx.fbo.viewport = (0, 0, w, h)
@@ -127,7 +56,7 @@ def run():
         # Update
         if (time - last_update) > 2:
             last_update = time
-            verts = vertices()
+            canvas.regenerate()
 
         # Render here, e.g. using moderngl
         # canvas.clear(background_color)
@@ -138,7 +67,7 @@ def run():
             # (math.sin(time + 10) + 1.0) / 2,
             # (math.sin(time + 30) + 1.0) / 2,
         )
-        canvas.plot(verts, type="line")
+        canvas.render()
 
         # Swap front and back buffers
         glfw.swap_buffers(window)
---
+++ src/python_glfw/screens/graph.py
@@ -0,0 +1,79 @@
+from typing import Any, Tuple
+import moderngl
+import numpy as np
+
+
+def vertices():
+    x = np.linspace(-1.0, 1.0, 50)
+    y = np.random.rand(50) - 0.5
+    r = np.ones(50)
+    g = np.arange(0, 1, 0.02)
+    b = np.zeros(50)
+    a = np.zeros(50)
+    return np.dstack([x, y, r, g, b, a])
+
+
+class Graph2DScreen:
+    def __init__(self, ctx: moderngl.Context):
+        self.ctx = ctx
+        self.verts = vertices()
+        self.prog = self.ctx.program(
+            vertex_shader='''
+                #version 330
+
+                uniform vec2 Pan;
+
+                in vec2 in_vert;
+                in vec4 in_color;
+
+                out vec4 v_color;
+
+                void main() {
+                    v_color = in_color;
+                    gl_Position = vec4(in_vert - Pan, 0.0, 1.0);
+                }
+            ''',
+            fragment_shader='''
+                #version 330
+
+                in vec4 v_color;
+
+                out vec4 f_color;
+
+                void main() {
+                    f_color = v_color;
+                }
+            ''',
+        )
+
+        self.vbo = ctx.buffer(reserve='4MB', dynamic=True)  # type: ignore
+        self.vao = ctx.simple_vertex_array(
+            self.prog, self.vbo, 'in_vert', 'in_color')
+
+    def pan(self, pos):
+        self.prog['Pan'].value = pos
+
+    def relative_pan(self, relative_pos):
+        pan = self.prog['Pan']
+        pos = pan.value
+        pan.value = (pos[0] + relative_pos[0], pos[1] + relative_pos[1])
+
+    def clear(self, color: Tuple[int, int, int, int] = (0, 0, 0, 0)):
+        self.ctx.clear(*color)
+
+    def plot(self, points: Any, type: str = 'line'):
+        data = points.astype('f4').tobytes()
+        self.vbo.orphan()
+        self.vbo.write(data)
+        if type == 'line':
+            self.ctx.line_width = 5.0
+            self.vao.render(moderngl.LINE_STRIP, vertices=len(data) // 24)
+        if type == 'points':
+            self.ctx.point_size = 3.0
+            self.vao.render(moderngl.POINTS, vertices=len(data) // 24)
+
+    def regenerate(self):
+        self.verts = vertices()
+
+    def render(self):
+        self.plot(self.verts)
